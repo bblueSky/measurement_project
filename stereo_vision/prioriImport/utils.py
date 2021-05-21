34da81d0ccedf8ca67c7556d06625d41fb1893e4
bbluesky <1286382894@qq.com>
2021-05-21 22:37:43 +0800

    基本完成了先验加工数据写入函数，数据格式还缺点东西，还差圆心坐标跟序列的位置没有加，之后还要完成拟合圆的函数

---
+++ stereo_vision/prioriImport/utils.py
@@ -0,0 +1,169 @@
+import numpy as np
+import os
+import time
+import json
+from xml.dom import minidom
+"""
+[['2021-5-19-22:42:12'],
+ ['850.00'], 
+ ['760.03'],
+ ['(0,411.05)', '(205.53,355.98)', '(355.98,205.53)', '(411.05,0.00)', '(355.98,-205.53)', '(205.53,-355.98)', '(0.00,-411.05)', '(-205.53,-355.98)', '(-355.98,-205.53)', '(-411.05,0)', '(-355.98,205.53)', '(-205.53,355.98)'],
+ ['(397.04,-106.39)', '(106.39,-397.04)', '(-106.39,-397.04)', '(-290.66,-290.66)', '(-106.39,397.04)'], 
+ ['(106.39,397.04)'], 
+ ['850.01'],
+ ['760.10'], 
+ ['(138.52,380.58)', '(260.33,310.25)', '(350.74,202.50)', '(398.85,70.33)', '(398.85,-70.33)', '(350.74,-202.50)', '(260.33,-310.25)', '(138.52,-380.58)', '(0,-405.00)', '(-138.52,-380.58)', '(-260.33,-310.25)', '(-350.74,-202.50)', '(-398.85,-70.33)', '(-398.85,70.33)', '(-350.74,202.50)', '(-260.33,310.25)', '(-138.52,380.58)', '(0,405.00)'], 
+ [], 
+ []]
+"""
+
+
+def prioriDataInput(res):  ##函数作用：1、把初始列表的数据写进xml 2、用json返回所有二维点的坐标数据
+    datatime = res[0][0]
+    firmPath = os.path.dirname(os.path.realpath(__file__)).replace("prioriImport","static/priori_data/")
+    savePath = firmPath+datatime+".xml"
+    p_doc = minidom.Document()
+    root = p_doc.createElement('priori_data')
+    p_doc.appendChild(root)
+    importTime = p_doc.createElement("importTime")
+    A_end = p_doc.createElement("A_end")
+    B_end = p_doc.createElement("B_end")
+    root.appendChild(importTime)
+    root.appendChild(A_end)
+    root.appendChild(B_end)
+
+    result = dict()
+    result["time"] = res[0][0]
+    time = p_doc.createElement("time")
+    time.appendChild(p_doc.createTextNode(res[0][0]))
+    importTime.appendChild(time)
+    result["AouterD"] = res[1][0]
+    AouterD = p_doc.createElement("AouterD")
+    diameter = p_doc.createElement("diameter")
+    diameter.appendChild(p_doc.createTextNode(res[1][0]))
+    AouterD.appendChild(diameter)
+    A_end.appendChild(AouterD)
+    result["AinnerD"] = res[2][0]
+    AinnerD = p_doc.createElement("AinnerD")
+    diameter = p_doc.createElement("diameter")
+    diameter.appendChild(p_doc.createTextNode(res[2][0]))
+    AinnerD.appendChild(diameter)
+    A_end.appendChild(AinnerD)
+    result["Amouting"] = list()
+    Amouting = p_doc.createElement("Amouting")
+    A_end.appendChild(Amouting)
+    i = 0
+    for s in res[3]:
+        p = p_doc.createElement("p"+str(i))
+        x,y = s[1:-1].split(',')
+        axis_x = p_doc.createElement("x")
+        axis_y = p_doc.createElement("y")
+        axis_x.appendChild(p_doc.createTextNode(x))
+        axis_y.appendChild(p_doc.createTextNode(y))
+        p.appendChild(axis_x)
+        p.appendChild(axis_y)
+        Amouting.appendChild(p)
+        i+=1
+        result["Amouting"].append([x,y])
+    result["Atapped"] = list()
+    Atapped = p_doc.createElement("Atapped")
+    A_end.appendChild(Atapped)
+    i = 0
+    for s in res[4]:
+        p = p_doc.createElement("p"+str(i))
+        x, y = s[1:-1].split(',')
+        axis_x = p_doc.createElement("x")
+        axis_y = p_doc.createElement("y")
+        axis_x.appendChild(p_doc.createTextNode(x))
+        axis_y.appendChild(p_doc.createTextNode(y))
+        p.appendChild(axis_x)
+        p.appendChild(axis_y)
+        Atapped.appendChild(p)
+        i+=1
+        result["Atapped"].append([x, y])
+    result["Aquadrant"] = list()
+    Aquadrant = p_doc.createElement("Aquadrant")
+    A_end.appendChild(Aquadrant)
+    i=0
+    for s in res[5]:
+        p = p_doc.createElement("p"+str(i))
+        x, y = s[1:-1].split(',')
+        axis_x = p_doc.createElement("x")
+        axis_y = p_doc.createElement("y")
+        axis_x.appendChild(p_doc.createTextNode(x))
+        axis_y.appendChild(p_doc.createTextNode(y))
+        p.appendChild(axis_x)
+        p.appendChild(axis_y)
+        Aquadrant.appendChild(p)
+        i+=1
+        result["Aquadrant"].append([x, y])
+    result["BouterD"] = res[6][0]
+    BouterD = p_doc.createElement("BouterD")
+    diameter = p_doc.createElement("diameter")
+    diameter.appendChild(p_doc.createTextNode(res[6][0]))
+    BouterD.appendChild(diameter)
+    B_end.appendChild(BouterD)
+    result["BinnerD"] = res[7][0]
+    BinnerD = p_doc.createElement("BinnerD")
+    diameter = p_doc.createElement("diameter")
+    diameter.appendChild(p_doc.createTextNode(res[7][0]))
+    BinnerD.appendChild(diameter)
+    B_end.appendChild(BinnerD)
+    result["Bmouting"] = list()
+    Bmouting = p_doc.createElement("Bmouting")
+    B_end.appendChild(Bmouting)
+    i=0
+    for s in res[8]:
+        p = p_doc.createElement("p" + str(i))
+        x, y = s[1:-1].split(',')
+        axis_x = p_doc.createElement("x")
+        axis_y = p_doc.createElement("y")
+        axis_x.appendChild(p_doc.createTextNode(x))
+        axis_y.appendChild(p_doc.createTextNode(y))
+        p.appendChild(axis_x)
+        p.appendChild(axis_y)
+        Bmouting.appendChild(p)
+        i+=1
+        result["Bmouting"].append([x, y])
+    result["Btapped"] = list()
+    Btapped = p_doc.createElement("Btapped")
+    B_end.appendChild(Btapped)
+    i = 0
+    for s in res[9]:
+        p = p_doc.createElement("p" + str(i))
+        x, y = s[1:-1].split(',')
+        axis_x = p_doc.createElement("x")
+        axis_y = p_doc.createElement("y")
+        axis_x.appendChild(p_doc.createTextNode(x))
+        axis_y.appendChild(p_doc.createTextNode(y))
+        p.appendChild(axis_x)
+        p.appendChild(axis_y)
+        Btapped.appendChild(p)
+        i += 1
+        result["Btapped"].append([x, y])
+    result["Bquadrant"] = list()
+    Bquadrant = p_doc.createElement("Bquadrant")
+    B_end.appendChild(Bquadrant)
+    i = 0
+    for s in res[10]:
+        p = p_doc.createElement("p" + str(i))
+        x, y = s[1:-1].split(',')
+        axis_x = p_doc.createElement("x")
+        axis_y = p_doc.createElement("y")
+        axis_x.appendChild(p_doc.createTextNode(x))
+        axis_y.appendChild(p_doc.createTextNode(y))
+        p.appendChild(axis_x)
+        p.appendChild(axis_y)
+        Bquadrant.appendChild(p)
+        i += 1
+        result["Bquadrant"].append([x, y])
+
+    fp = open(savePath, 'w')
+    p_doc.writexml(fp, indent='\t', addindent='\t', newl='\n', encoding="utf-8")
+    return result
+
+
+def dataFitCircle():  ##函数作用：1、读取xml里的数据 2、根据这些数据拟合圆，计算各种孔位序列 3、用json返回圆心坐标 4、把圆心坐标、孔位序列写入xml
+
+
+    return 0
--- stereo_vision/prioriImport/views.py
+++ stereo_vision/prioriImport/views.py
@@ -6,7 +6,7 @@ import time
 import json
 import datetime
 import os
-
+from stereo_vision.prioriImport.utils import prioriDataInput,dataFitCircle
 
 
 
@@ -18,13 +18,30 @@ def index():
 
 @prioriImport.route('/prioriFile/',methods=['POST','GET'])
 def prioriFile():
-    filePath = os.path.dirname(os.path.realpath(__file__)).replace("prioriImport","exteriorFile/PF.txt")
-    # with open(filePath, "r") as f:
+    filePath = os.path.dirname(os.path.realpath(__file__)).replace("prioriImport","exteriorFile/PF.txt") ##先用固定路径，以后再考虑文件传输的事
+    formPath = os.path.dirname(os.path.realpath(__file__)).replace("prioriImport","static/priori_data/")
+    cur = list() ##每种数据的列表
+    res = list() ##总列表
+    with open(filePath, "r") as f:
+        for line in f.readlines():
+            line = line.strip("\n")
+            if line=="数据时间":
+                continue
+            if line=="A端法兰外圈直径" or line=="A端法兰内圈直径" or line=="A端安装孔" or line=="A端螺纹孔" or line=="A端象限孔" or line=="B端法兰外圈直径" or line=="B端法兰内圈直径" or line=="B端安装孔" or line=="B端螺纹孔" or line=="B端象限孔":
+                res.append(cur)
+                cur = list()
+                continue
+            cur.append(line)
+        res.append(cur)
+    # print(res)
+    result = prioriDataInput(res)  ##函数作用：1、把初始列表的数据写进xml 2、用json返回所有二维点的坐标数据
+    print(result)
+    return result
 
-    return str(1)
 
+@prioriImport.route('/fitCircle/',methods=['POST','GET'])
+def fitCircle():
+    res = dataFitCircle()  ##函数作用：1、读取xml里的数据 2、根据这些数据拟合圆 3、用json返回圆心坐标 4、把圆心坐标写入xml
+    return res
 
-
-
-
-
+##还差一个函数
