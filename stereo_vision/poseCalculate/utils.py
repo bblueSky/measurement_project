8588f3dc4296d8e071ba4c22b1a469246cb8e196
bbluesky <1286382894@qq.com>
2021-06-01 22:34:03 +0800

    完成了整个筒段A、B端形心坐标的全局坐标求解，下一步求轴线三维方程、AB端象限偏角；已经拿到通讯代码，尝试集成进软件中；使用新图像训练网络

--- stereo_vision/poseCalculate/utils.py
+++ stereo_vision/poseCalculate/utils.py
@@ -244,12 +244,12 @@ def tubePoseCalculate(filePath,dataPath):
         centerAndAngle[0, 1] = d_center_Y
         centerAndAngle[1, 0] = aft_d_Amouting_mat[-1,0]
         centerAndAngle[1, 1] = aft_d_Amouting_mat[-1,1]
-        print(centerAndAngle)
+        # print(centerAndAngle)
         R_d2p, T_d2p = rigid_transform_3D(aft_d_Amouting_mat,aft_p_Amouting_mat2)
         n = 2
-        centerAndAngle2 = (R_d2p * centerAndAngle.T) + np.tile(T_d2p, (1, n))
-        centerAndAngle2 = centerAndAngle2.T
-        print(centerAndAngle2) ## 包含A端形心点与正安装孔全局坐标的2x3矩阵
+        A_centerAndAngle2 = (R_d2p * centerAndAngle.T) + np.tile(T_d2p, (1, n))
+        A_centerAndAngle2 = A_centerAndAngle2.T
+        print(A_centerAndAngle2) ## 包含A端形心点与正安装孔全局坐标的2x3矩阵
     else:
         ##无象限孔情况，区别在于靶标会装在正安装孔右边最近的安装孔上
         if numsOfAmouting+numsOfAtapped+numsOfAquadrant!=numsOfAhole+1:
@@ -375,10 +375,8 @@ def tubePoseCalculate(filePath,dataPath):
         last_point[1] = aft_d_Amouting_mat[0,1]
         last_point[2] = aft_d_Amouting_mat[0,2]
         aft_d_Amouting_mat = np.delete(aft_d_Amouting_mat,0,axis=0)
-        aft_d_Amouting_mat = np.stack((aft_d_Amouting_mat,last_point),axis=0)
-
-
-
+        ind = len(aft_d_Amouting_mat)
+        aft_d_Amouting_mat = np.insert(aft_d_Amouting_mat,ind,values=last_point,axis=0)
         # print(d_index_Amouting)
         # print(aft_d_Amouting_mat)
         ##已检验
@@ -387,30 +385,350 @@ def tubePoseCalculate(filePath,dataPath):
         centerAndAngle[0, 1] = d_center_Y
         centerAndAngle[1, 0] = aft_d_Amouting_mat[-1, 0]
         centerAndAngle[1, 1] = aft_d_Amouting_mat[-1, 1]
-        print(centerAndAngle)
+        # print(centerAndAngle)
         R_d2p, T_d2p = rigid_transform_3D(aft_d_Amouting_mat, aft_p_Amouting_mat2)
         n = 2
-        centerAndAngle2 = (R_d2p * centerAndAngle.T) + np.tile(T_d2p, (1, n))
-        centerAndAngle2 = centerAndAngle2.T
-        print(centerAndAngle2)  ## 包含A端形心点与象限孔全局坐标的2x3矩阵
+        A_centerAndAngle2 = (R_d2p * centerAndAngle.T) + np.tile(T_d2p, (1, n))
+        A_centerAndAngle2 = A_centerAndAngle2.T
+        print(A_centerAndAngle2)  ## 包含A端形心点与象限孔全局坐标的2x3矩阵
+    ##注意这块只处理B端
+    Bmouting = d_root.getElementsByTagName('Bmouting')[0]
+    Btapped = d_root.getElementsByTagName('Btapped')[0]
+    Bquadrant = d_root.getElementsByTagName('Bquadrant')[0]
+    numsOfBmouting = len(Bmouting.childNodes)
+    numsOfBtapped = len(Btapped.childNodes)
+    numsOfBquadrant = len(Bquadrant.childNodes)
+    # print("numsOfBmouting:"+str(numsOfBmouting))
+    # print("numsOfBtapped:"+str(numsOfBtapped))
+    # print("numsOfBquadrant:"+str(numsOfBquadrant))
+    ##处理计算值
+    Bhole = p_root.getElementsByTagName("threeD")[2]
+    Btarget = p_root.getElementsByTagName("threeD")[3]
+    numsOfBhole = len(Bhole.childNodes)
+    numsOfBtarget = len(Btarget.childNodes)  ## 此处默认检测到的靶标数量为4(3个板上1个筒上)
+    # print("numsOfBhole:"+str(numsOfBhole))
+    data_list = list()
+    data_index_list = list()
+    for i in range(numsOfBmouting):
+        x = Bmouting.getElementsByTagName('p' + str(i))[0].childNodes[0].childNodes[0].data
+        y = Bmouting.getElementsByTagName('p' + str(i))[0].childNodes[1].childNodes[0].data
+        data_list.append([float(x), float(y)])
+        data_index_list.append(i)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    hole_list = list()
+    hole_index_list = list()
+    hole_r_list = list()
+    for i in range(numsOfBhole):
+        x = Bhole.getElementsByTagName('point' + str(i))[0].childNodes[0].childNodes[0].data
+        y = Bhole.getElementsByTagName('point' + str(i))[0].childNodes[1].childNodes[0].data
+        z = Bhole.getElementsByTagName('point' + str(i))[0].childNodes[2].childNodes[0].data
+        r = Bhole.getElementsByTagName('point' + str(i))[0].childNodes[3].childNodes[0].data
+        hole_list.append([float(x), float(y), float(z)])
+        hole_index_list.append(i)
+        hole_r_list.append(float(r))
+    # print(data_list)
+    # print(data_index_list)
+    # print(hole_list)
+    # print(hole_index_list)
+    # print(hole_r_list)
+    ##已验证
+    target_list = list()
+    target_index_list = list()
+    ##生成靶标列表的同时找出离群靶标即外靶标
+    record_i = 0
+    max_y = float('-inf')
+    for i in range(numsOfBtarget):
+        x = Btarget.getElementsByTagName('point' + str(i))[0].childNodes[0].childNodes[0].data
+        y = float(Btarget.getElementsByTagName('point' + str(i))[0].childNodes[1].childNodes[0].data)
+        z = Btarget.getElementsByTagName('point' + str(i))[0].childNodes[2].childNodes[0].data
+        target_list.append([float(x), float(y), float(z)])
+        target_index_list.append(i)
+        if y > max_y:
+            max_y = y
+            record_i = i
+    # print(target_list)
+    # print(target_index_list)
+    distence = d = 0
+    Outlier_i = 0
+    for i in range(numsOfBtarget):
+        d = (target_list[i][0] - target_list[record_i][0]) ** 2 + (
+                    target_list[i][1] - target_list[record_i][1]) ** 2 + (
+                        target_list[i][2] - target_list[record_i][2]) ** 2
+        if d > distence:
+            Outlier_i = i
+            distence = d
+    # print("离群点与板上最下靶标距离："+str(math.sqrt(distence)))
+    # print("外标在index中id："+str(Outlier_i))
+    # print("外标坐标："+str(target_list[Outlier_i]))
+    ##已验证
+    ##分情况讨论
+    if numsOfBquadrant != 0:
+        ##有象限孔
+        if numsOfBmouting + numsOfBtapped + numsOfBquadrant != numsOfBhole + 1:
+            return False
+        ##根据孔径估算排名得出安装孔实测列表
+        hole_index_list = np.argsort(hole_r_list)[::-1]
+        hole_index_list = hole_index_list[:numsOfBmouting]
+        p_Bmouting = list()
+        p_Bmouting_mat = np.mat(np.zeros([numsOfBmouting, 3]))
+        for i in hole_index_list:
+            p_Bmouting.append(hole_list[i])  ##得出计算值中的安装孔列表
+            p_Bmouting_mat[i, 0] = hole_list[i][0]
+            p_Bmouting_mat[i, 1] = hole_list[i][1]
+            p_Bmouting_mat[i, 2] = hole_list[i][2]
+        mu_p_Bmouting = np.mean(p_Bmouting_mat, axis=0)
+        # print("计算值质心坐标："+str(mu_p_Bmouting))
+        # print("安装孔数量："+str(len(p_Bmouting)))
+        fir_vector = target_list[Outlier_i] - mu_p_Bmouting
+        # print("外标向量："+str(fir_vector))
+        ##已检验
+        ##定义偏角列表
+        angle_list = list()
+        for i in range(len(p_Bmouting)):
+            angleOfmouting = GetClockAngle(np.squeeze(np.asarray(p_Bmouting[i] - mu_p_Bmouting)),
+                                           np.squeeze(np.asarray(fir_vector)))
+            angle_list.append(angleOfmouting)
+        # print("安装孔偏角列表"+str(angle_list))
+        ##已验证
+        p_index_Bmouting = np.argsort(angle_list)[::-1]  ##因为y轴相反，所以降序代替升序
+        aft_p_Bmouting_mat = np.mat(np.zeros([len(p_Bmouting), 3]))
+        for i in range(len(p_Bmouting)):
+            aft_p_Bmouting_mat[i, 0] = p_Bmouting[p_index_Bmouting[i]][0]
+            aft_p_Bmouting_mat[i, 1] = p_Bmouting[p_index_Bmouting[i]][1]
+            aft_p_Bmouting_mat[i, 2] = p_Bmouting[p_index_Bmouting[i]][2]
+        # print(p_index_Bmouting)
+        # print(aft_p_Bmouting_mat)
+        ##已验证
+        record_target = target_list[record_i]
+        target_list.pop(Outlier_i)
+        p_width_index = 0
+        # print(target_list)
+        ##找出每个靶标的身份
+        distence = float("-inf")
+        p_height_index = 0
+        for i in range(len(target_list)):
+            d = (target_list[i][0] - record_target[0]) ** 2 + (target_list[i][1] - record_target[1]) ** 2 + (
+                        target_list[i][2] - record_target[2]) ** 2
+            if d > distence:
+                distence = d
+                p_height_index = i
+            if d == 0:
+                p_width_index = i
+        p_origin_index = 3 - (p_width_index + p_height_index)
+        # print("BPH"+str(target_list[p_height_index]))
+        # print("BPW"+str(target_list[p_width_index]))
+        # print("BPO"+str(target_list[p_origin_index]))
+        ##已检验
+        BPOs = g_root.getElementsByTagName("BPOs")[0]
+        BPHs = g_root.getElementsByTagName("BPHs")[0]
+        BPWs = g_root.getElementsByTagName("BPWs")[0]
+        BPOs_X = float(BPOs.getElementsByTagName("X")[0].childNodes[0].data)
+        BPOs_Y = float(BPOs.getElementsByTagName("Y")[0].childNodes[0].data)
+        BPOs_Z = float(BPOs.getElementsByTagName("Z")[0].childNodes[0].data)
+        BPHs_X = float(BPHs.getElementsByTagName("X")[0].childNodes[0].data)
+        BPHs_Y = float(BPHs.getElementsByTagName("Y")[0].childNodes[0].data)
+        BPHs_Z = float(BPHs.getElementsByTagName("Z")[0].childNodes[0].data)
+        BPWs_X = float(BPWs.getElementsByTagName("X")[0].childNodes[0].data)
+        BPWs_Y = float(BPWs.getElementsByTagName("Y")[0].childNodes[0].data)
+        BPWs_Z = float(BPWs.getElementsByTagName("Z")[0].childNodes[0].data)
+        Btarget_mat = np.mat([[BPOs_X, BPOs_Y, BPOs_Z], [BPHs_X, BPHs_Y, BPHs_Z], [BPWs_X, BPWs_Y, BPWs_Z]])
+        p_Btarget_mat = np.mat(
+            [[target_list[p_origin_index][0], target_list[p_origin_index][1], target_list[p_origin_index][2]],
+             [target_list[p_height_index][0], target_list[p_height_index][1], target_list[p_height_index][2]],
+             [target_list[p_width_index][0], target_list[p_width_index][1], target_list[p_width_index][2]]])
+        # print(Btarget_mat)
+        # print(p_Btarget_mat)
+        ##已检验
+        # print(aft_p_Bmouting_mat)
+        R_p2g, T_p2g = rigid_transform_3D(p_Btarget_mat, Btarget_mat)
+        n = len(aft_p_Bmouting_mat)
+        aft_p_Bmouting_mat2 = (R_p2g * aft_p_Bmouting_mat.T) + np.tile(T_p2g, (1, n))
+        aft_p_Bmouting_mat2 = aft_p_Bmouting_mat2.T
+        # print(aft_p_Bmouting_mat2)
+        ##以上完成了安装孔按角度排序且转换至全局坐标系
+        d_Bmouting = list()
+        for i in range(len(data_list)):
+            d_Bmouting.append([data_list[i][0], data_list[i][1], 0])
+        # print(d_Bmouting)
+        centerAndAngle = np.mat(np.zeros([2, 3]))
+        d_center = d_root.getElementsByTagName("center")[1]
+        d_center_X = float(d_center.getElementsByTagName("x")[0].childNodes[0].data)
+        d_center_Y = float(d_center.getElementsByTagName("y")[0].childNodes[0].data)
+        mu_Bmouting = np.mat([d_center_X, d_center_Y, 0])
+        # print(mu_Bmouting)
+        targetBquadrant = [float(Bquadrant.getElementsByTagName("x")[0].childNodes[0].data),
+                           float(Bquadrant.getElementsByTagName("y")[0].childNodes[0].data), 0]
+        d_fir_vector = targetBquadrant - mu_Bmouting
+        # print(d_fir_vector)
+        ##已检验
+        d_angle_list = list()
+        for i in range(len(d_Bmouting)):
+            angleOfmouting = GetClockAngle(np.squeeze(np.asarray(d_Bmouting[i] - mu_Bmouting)),
+                                           np.squeeze(np.asarray(d_fir_vector)))
+            d_angle_list.append(angleOfmouting)
+        # print("安装孔偏角列表"+str(d_angle_list))
+        d_index_Bmouting = np.argsort(d_angle_list)
+        aft_d_Bmouting_mat = np.mat(np.zeros([len(d_Bmouting), 3]))
+        for i in range(len(d_Bmouting)):
+            aft_d_Bmouting_mat[i, 0] = d_Bmouting[d_index_Bmouting[i]][0]
+            aft_d_Bmouting_mat[i, 1] = d_Bmouting[d_index_Bmouting[i]][1]
+            aft_d_Bmouting_mat[i, 2] = d_Bmouting[d_index_Bmouting[i]][2]
+        # print(d_index_Bmouting)
+        # print(aft_d_Bmouting_mat)
+        ##已检验
+        ##下一步求d2p转换关系
+        centerAndAngle[0, 0] = d_center_X
+        centerAndAngle[0, 1] = d_center_Y
+        centerAndAngle[1, 0] = aft_d_Bmouting_mat[-1, 0]
+        centerAndAngle[1, 1] = aft_d_Bmouting_mat[-1, 1]
+        # print(centerAndAngle)
+        R_d2p, T_d2p = rigid_transform_3D(aft_d_Bmouting_mat, aft_p_Bmouting_mat2)
+        n = 2
+        B_centerAndAngle2 = (R_d2p * centerAndAngle.T) + np.tile(T_d2p, (1, n))
+        B_centerAndAngle2 = B_centerAndAngle2.T
+        print(B_centerAndAngle2)  ## 包含A端形心点与正安装孔全局坐标的2x3矩阵
+        ##先验证B端有象限孔的情况
+    else:
+        ##无象限孔情况，区别在于靶标会装在正安装孔右边最近的安装孔上
+        if numsOfBmouting + numsOfBtapped + numsOfBquadrant != numsOfBhole + 1:
+            return False
+        hole_index_list = np.argsort(hole_r_list)[::-1]
+        hole_index_list = hole_index_list[:numsOfBmouting - 1]
+        p_Bmouting = list()
+        p_Bmouting_mat = np.mat(np.zeros([numsOfBmouting, 3]))
+        for i in hole_index_list:
+            p_Bmouting.append(hole_list[i])  ##得出计算值中的安装孔列表
+            p_Bmouting_mat[i, 0] = hole_list[i][0]
+            p_Bmouting_mat[i, 1] = hole_list[i][1]
+            p_Bmouting_mat[i, 2] = hole_list[i][2]
+        p_Bmouting_mat[-1, 0] = target_list[Outlier_i][0]
+        p_Bmouting_mat[-1, 1] = target_list[Outlier_i][1]
+        p_Bmouting_mat[-1, 2] = target_list[Outlier_i][2]
+        mu_p_Bmouting = np.mean(p_Bmouting_mat, axis=0)
+        # print("计算值质心坐标："+str(mu_p_Bmouting))
+        # print("安装孔数量："+str(len(p_Bmouting)))
+        fir_vector = target_list[Outlier_i] - mu_p_Bmouting
+        # print("外标向量："+str(fir_vector))
+        ##已检验
+        ##定义偏角列表
+        angle_list = list()
+        for i in range(len(p_Bmouting)):
+            angleOfmouting = GetClockAngle(np.squeeze(np.asarray(p_Bmouting[i] - mu_p_Bmouting)),
+                                           np.squeeze(np.asarray(fir_vector)))
+            angle_list.append(angleOfmouting)
+        # print("安装孔偏角列表"+str(angle_list))
+        ##已验证
+        p_index_Bmouting = np.argsort(angle_list)[::-1]  ##因为y轴相反，所以降序代替升序
+        aft_p_Bmouting_mat = np.mat(np.zeros([len(p_Bmouting), 3]))
+        for i in range(len(p_Bmouting)):
+            aft_p_Bmouting_mat[i, 0] = p_Bmouting[p_index_Bmouting[i]][0]
+            aft_p_Bmouting_mat[i, 1] = p_Bmouting[p_index_Bmouting[i]][1]
+            aft_p_Bmouting_mat[i, 2] = p_Bmouting[p_index_Bmouting[i]][2]
+        # print(p_index_Bmouting)
+        # print(aft_p_Bmouting_mat)
+        ##已验证
+        record_target = target_list[record_i]
+        target_list.pop(Outlier_i)
+        p_width_index = 0
+        # print(target_list)
+        ##找出每个靶标的身份
+        distence = float("-inf")
+        p_height_index = 0
+        for i in range(len(target_list)):
+            d = (target_list[i][0] - record_target[0]) ** 2 + (target_list[i][1] - record_target[1]) ** 2 + (
+                    target_list[i][2] - record_target[2]) ** 2
+            if d > distence:
+                distence = d
+                p_height_index = i
+            if d == 0:
+                p_width_index = i
+        p_origin_index = 3 - (p_width_index + p_height_index)
+        # print("BPH"+str(target_list[p_height_index]))
+        # print("BPW"+str(target_list[p_width_index]))
+        # print("BPO"+str(target_list[p_origin_index]))
+        ##已检验
+        BPOs = g_root.getElementsByTagName("BPOs")[0]
+        BPHs = g_root.getElementsByTagName("BPHs")[0]
+        BPWs = g_root.getElementsByTagName("BPWs")[0]
+        BPOs_X = float(BPOs.getElementsByTagName("X")[0].childNodes[0].data)
+        BPOs_Y = float(BPOs.getElementsByTagName("Y")[0].childNodes[0].data)
+        BPOs_Z = float(BPOs.getElementsByTagName("Z")[0].childNodes[0].data)
+        BPHs_X = float(BPHs.getElementsByTagName("X")[0].childNodes[0].data)
+        BPHs_Y = float(BPHs.getElementsByTagName("Y")[0].childNodes[0].data)
+        BPHs_Z = float(BPHs.getElementsByTagName("Z")[0].childNodes[0].data)
+        BPWs_X = float(BPWs.getElementsByTagName("X")[0].childNodes[0].data)
+        BPWs_Y = float(BPWs.getElementsByTagName("Y")[0].childNodes[0].data)
+        BPWs_Z = float(BPWs.getElementsByTagName("Z")[0].childNodes[0].data)
+        Btarget_mat = np.mat([[BPOs_X, BPOs_Y, BPOs_Z], [BPHs_X, BPHs_Y, BPHs_Z], [BPWs_X, BPWs_Y, BPWs_Z]])
+        p_Btarget_mat = np.mat(
+            [[target_list[p_origin_index][0], target_list[p_origin_index][1], target_list[p_origin_index][2]],
+             [target_list[p_height_index][0], target_list[p_height_index][1], target_list[p_height_index][2]],
+             [target_list[p_width_index][0], target_list[p_width_index][1], target_list[p_width_index][2]]])
+        # print(Btarget_mat)
+        # print(p_Btarget_mat)
+        ##已检验
+        # print(aft_p_Bmouting_mat)
+        R_p2g, T_p2g = rigid_transform_3D(p_Btarget_mat, Btarget_mat)
+        n = len(aft_p_Bmouting_mat)
+        aft_p_Bmouting_mat2 = (R_p2g * aft_p_Bmouting_mat.T) + np.tile(T_p2g, (1, n))
+        aft_p_Bmouting_mat2 = aft_p_Bmouting_mat2.T
+        # print(aft_p_Bmouting_mat2)
+        ##以上完成了安装孔按角度排序且转换至全局坐标系
+        d_Bmouting = list()
+        max_y = float("-inf")
+        d_fir_index = 0
+        for i in range(len(data_list)):
+            d_Bmouting.append([data_list[i][0], data_list[i][1], 0])
+            if data_list[i][1] > max_y:
+                max_y = data_list[i][1]
+                d_fir_index = i
+        ##没有象限孔的话需要排序后去掉偏角最接近零度的点
+        # print(d_Bmouting)
+        centerAndAngle = np.mat(np.zeros([2, 3]))
+        d_center = d_root.getElementsByTagName("center")[1]
+        d_center_X = float(d_center.getElementsByTagName("x")[0].childNodes[0].data)
+        d_center_Y = float(d_center.getElementsByTagName("y")[0].childNodes[0].data)
+        mu_Bmouting = np.mat([d_center_X, d_center_Y, 0])
+        ##注意这里出现大变化！！！基准向量是正安装孔index0！！！
+        holeBquadrant = [data_list[d_fir_index][0], data_list[d_fir_index][1], 0]
+        d_fir_vector = holeBquadrant - mu_Bmouting
+        # print(d_fir_vector)
+        ##已检验
+        d_angle_list = list()
+        for i in range(len(d_Bmouting)):
+            angleOfmouting = GetClockAngle(np.squeeze(np.asarray(d_Bmouting[i] - mu_Bmouting)),
+                                           np.squeeze(np.asarray(d_fir_vector)))
+            d_angle_list.append(angleOfmouting)
+        # print("安装孔偏角列表"+str(d_angle_list))
+        d_index_Bmouting = np.argsort(d_angle_list)
+        d_index_Bmouting = np.delete(d_index_Bmouting, 1)
+        aft_d_Bmouting_mat = np.mat(np.zeros([len(d_Bmouting) - 1, 3]))
+        for i in range(len(d_Bmouting) - 1):
+            aft_d_Bmouting_mat[i, 0] = d_Bmouting[d_index_Bmouting[i]][0]
+            aft_d_Bmouting_mat[i, 1] = d_Bmouting[d_index_Bmouting[i]][1]
+            aft_d_Bmouting_mat[i, 2] = d_Bmouting[d_index_Bmouting[i]][2]
+        ##与计算数据一致，把正安装孔放在最后
+        last_point = np.array([0, 0, 0])
+        last_point[0] = aft_d_Bmouting_mat[0, 0]
+        last_point[1] = aft_d_Bmouting_mat[0, 1]
+        last_point[2] = aft_d_Bmouting_mat[0, 2]
+        aft_d_Bmouting_mat = np.delete(aft_d_Bmouting_mat, 0, axis=0)
+        ind = len(aft_d_Bmouting_mat)
+        aft_d_Bmouting_mat = np.insert(aft_d_Bmouting_mat, ind, values=last_point, axis=0)
+        # print(d_index_Bmouting)
+        # print(aft_d_Bmouting_mat)
+        ##已检验
+        ##下一步求d2p转换关系
+        centerAndAngle[0, 0] = d_center_X
+        centerAndAngle[0, 1] = d_center_Y
+        centerAndAngle[1, 0] = aft_d_Bmouting_mat[-1, 0]
+        centerAndAngle[1, 1] = aft_d_Bmouting_mat[-1, 1]
+        # print(centerAndAngle)
+        R_d2p, T_d2p = rigid_transform_3D(aft_d_Bmouting_mat, aft_p_Bmouting_mat2)
+        n = 2
+        B_centerAndAngle2 = (R_d2p * centerAndAngle.T) + np.tile(T_d2p, (1, n))
+        B_centerAndAngle2 = B_centerAndAngle2.T
+        print(B_centerAndAngle2)  ## 包含A端形心点与象限孔全局坐标的2x3矩阵
 
 
     return str(1)
---
+++ stereo_vision/robotMachine/slave.py
@@ -0,0 +1,114 @@
+import sys
+import modbus_tk
+import modbus_tk.defines as cst
+from modbus_tk import modbus_tcp
+import random
+import time
+
+def main() :
+
+	"""main"""
+
+	logger = modbus_tk.utils.create_logger(name = "console", record_format = "%(message)s")
+
+	try :
+
+		#Create the server
+		server = modbus_tcp.TcpServer(address="0.0.0.0", port=502)
+		#server.set_timeout(5.0)
+		logger.info("running...")
+		logger.info("enter 'quit' for closing the server")
+		server.start()
+		slave_1 = server.add_slave(1)
+		slave_1.add_block('A', cst.HOLDING_REGISTERS, 0, 10)
+		slave_1.set_values('A', 0, 10*[123])
+		while True:
+			slave_1.set_values('A', 0, 10*[random.randint(0,200)])  # 改变在地址0处的寄存器的值
+			time.sleep(1)
+
+		while True:
+			cmd = sys.stdin.readline()
+			args = cmd.split(' ')
+
+			if cmd.find('quit') == 0 :
+
+				sys.stdout.write('bye-bye\r\n')
+
+				break
+
+			elif args[0] == 'add_slave':
+
+				slave_id = int(args[1])
+
+				server.add_slave(slave_id)
+
+				sys.stdout.write('done: slave %d added\r\n' % slave_id)
+
+			elif args[0] == 'add_block':
+
+				slave_id = int(args[1])
+
+				name = args[2]
+
+				block_type = int(args[3])
+
+				starting_address = int(args[4])
+
+				length = int(args[5])
+
+				slave = server.get_slave(slave_id)
+
+				slave.add_block(name, block_type, starting_address, length)
+
+				sys.stdout.write('done: block %s added\r\n' % name)
+
+			elif args[0] == 'set_values':
+
+				slave_id = int(args[1])
+
+				name = args[2]
+
+				address = int(args[3])
+
+				values = []
+
+				for val in args[4:]:
+
+					values.append(int(val))
+
+				slave = server.get_slave(slave_id)
+
+				slave.set_values(name, address, values)
+
+				values = slave.get_values(name, address, len(values))
+
+				sys.stdout.write('done: values written: %s\r\n' % str(values))
+
+			elif args[0] == 'get_values':
+
+				slave_id = int(args[1])
+
+				name = args[2]
+
+				address = int(args[3])
+
+				length = int(args[4])
+
+				slave = server.get_slave(slave_id)
+
+				values = slave.get_values(name, address, length)
+
+				sys.stdout.write('done: values read: %s\r\n' % str(values))
+
+			else:
+
+				sys.stdout.write("unknown command %s\r\n" % args[0])
+
+	finally :
+
+		server.stop()
+
+if __name__ == "__main__" :
+
+	main()
+
