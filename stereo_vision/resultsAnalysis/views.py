b0c577bc21020cebc2fd911e5cdae28d20050a91
bbluesky <1286382894@qq.com>
2021-12-15 17:11:44 +0800

    大体上做完了第一项，还需要约束一下有效小数

--- stereo_vision/resultsAnalysis/views.py
+++ stereo_vision/resultsAnalysis/views.py
@@ -2,16 +2,12 @@
 
 from flask  import render_template, request
 from stereo_vision.resultsAnalysis import  resultsAnalysis
-import time
 import json
-import cv2
 import numpy as np
 import time
 import datetime
-import signal
-import threading
 import os
-import stereo_vision.ksj.cam as  kcam
+from xml.dom import minidom
 
 from  stereo_vision.cameraCalibration.utils  import  sig_calibration,stereo_Calibration
 
@@ -43,13 +39,89 @@ def selectLog():
 @resultsAnalysis.route('/confirmLog/', methods=['POST', 'GET'])
 def confirmLog():
     flag = request.args.get('flag')
-    print(flag)
+    # print(flag)
     ##拿到flag后开始根据这个索引收集数据
+    global_path = os.path.dirname(os.path.realpath(__file__)).replace("resultsAnalysis", "static/global_info.xml")
+    point_path = os.path.dirname(os.path.realpath(__file__)).replace("resultsAnalysis", "static/res_pictures/result/"+flag+"/points_info.xml")
+    pose_path = os.path.dirname(os.path.realpath(__file__)).replace("resultsAnalysis", "static/res_pictures/pose_result/"+flag+".xml")
+
+    global_doc = minidom.parse(global_path)
+    global_root = global_doc.documentElement
+
+    point_doc = minidom.parse(point_path)
+    point_root = point_doc.documentElement
+
+    pose_doc = minidom.parse(pose_path)
+    pose_root = pose_doc.documentElement
+
+    ##需要拿的数据：1、全局坐标系12个点 2、两端三维点 3、位姿四件套
+    APOX = global_root.getElementsByTagName("APO")[0].childNodes[0].childNodes[0].data
+    APOY = global_root.getElementsByTagName("APO")[0].childNodes[1].childNodes[0].data
+    APOZ = global_root.getElementsByTagName("APO")[0].childNodes[2].childNodes[0].data
+
+    APHX = global_root.getElementsByTagName("APH")[0].childNodes[0].childNodes[0].data
+    APHY = global_root.getElementsByTagName("APH")[0].childNodes[1].childNodes[0].data
+    APHZ = global_root.getElementsByTagName("APH")[0].childNodes[2].childNodes[0].data
+
+    APWX = global_root.getElementsByTagName("APW")[0].childNodes[0].childNodes[0].data
+    APWY = global_root.getElementsByTagName("APW")[0].childNodes[1].childNodes[0].data
+    APWZ = global_root.getElementsByTagName("APW")[0].childNodes[2].childNodes[0].data
+
+    BPOX = global_root.getElementsByTagName("BPO")[0].childNodes[0].childNodes[0].data
+    BPOY = global_root.getElementsByTagName("BPO")[0].childNodes[1].childNodes[0].data
+    BPOZ = global_root.getElementsByTagName("BPO")[0].childNodes[2].childNodes[0].data
+
+    BPHX = global_root.getElementsByTagName("BPH")[0].childNodes[0].childNodes[0].data
+    BPHY = global_root.getElementsByTagName("BPH")[0].childNodes[1].childNodes[0].data
+    BPHZ = global_root.getElementsByTagName("BPH")[0].childNodes[2].childNodes[0].data
+
+    BPWX = global_root.getElementsByTagName("BPW")[0].childNodes[0].childNodes[0].data
+    BPWY = global_root.getElementsByTagName("BPW")[0].childNodes[1].childNodes[0].data
+    BPWZ = global_root.getElementsByTagName("BPW")[0].childNodes[2].childNodes[0].data
 
 
 
 
-    return {"dd":flag}
+
+    APOXs = global_root.getElementsByTagName("APOs")[0].childNodes[0].childNodes[0].data
+    APOYs = global_root.getElementsByTagName("APOs")[0].childNodes[1].childNodes[0].data
+    APOZs = global_root.getElementsByTagName("APOs")[0].childNodes[2].childNodes[0].data
+
+    APHXs = global_root.getElementsByTagName("APHs")[0].childNodes[0].childNodes[0].data
+    APHYs = global_root.getElementsByTagName("APHs")[0].childNodes[1].childNodes[0].data
+    APHZs = global_root.getElementsByTagName("APHs")[0].childNodes[2].childNodes[0].data
+
+    APWXs = global_root.getElementsByTagName("APWs")[0].childNodes[0].childNodes[0].data
+    APWYs = global_root.getElementsByTagName("APWs")[0].childNodes[1].childNodes[0].data
+    APWZs = global_root.getElementsByTagName("APWs")[0].childNodes[2].childNodes[0].data
+
+    BPOXs = global_root.getElementsByTagName("BPOs")[0].childNodes[0].childNodes[0].data
+    BPOYs = global_root.getElementsByTagName("BPOs")[0].childNodes[1].childNodes[0].data
+    BPOZs = global_root.getElementsByTagName("BPOs")[0].childNodes[2].childNodes[0].data
+
+    BPHXs = global_root.getElementsByTagName("BPHs")[0].childNodes[0].childNodes[0].data
+    BPHYs = global_root.getElementsByTagName("BPHs")[0].childNodes[1].childNodes[0].data
+    BPHZs = global_root.getElementsByTagName("BPHs")[0].childNodes[2].childNodes[0].data
+
+    BPWXs = global_root.getElementsByTagName("BPWs")[0].childNodes[0].childNodes[0].data
+    BPWYs = global_root.getElementsByTagName("BPWs")[0].childNodes[1].childNodes[0].data
+    BPWZs = global_root.getElementsByTagName("BPWs")[0].childNodes[2].childNodes[0].data
+
+    res = {
+        "APO": [APOX, APOY, APOZ],
+        "APH": [APHX, APHY, APHZ],
+        "APW": [APWX, APWY, APWZ],
+        "BPO": [BPOX, BPOY, BPOZ],
+        "BPH": [BPHX, BPHY, BPHZ],
+        "BPW": [BPWX, BPWY, BPWZ],
+        "APOs": [APOXs, APOYs, APOZs],
+        "APHs": [APHXs, APHYs, APHZs],
+        "APWs": [APWXs, APWYs, APWZs],
+        "BPOs": [BPOXs, BPOYs, BPOZs],
+        "BPHs": [BPHXs, BPHYs, BPHZs],
+        "BPWs": [BPWXs, BPWYs, BPWZs]
+    }
+    return res
 
 
 
