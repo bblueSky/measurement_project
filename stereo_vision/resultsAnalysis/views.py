f8b468e264d2af0622719f9769ff966b757eed21
bbluesky <1286382894@qq.com>
2021-12-15 11:41:16 +0800

    做完了几个按钮逻辑

--- stereo_vision/resultsAnalysis/views.py
+++ stereo_vision/resultsAnalysis/views.py
@@ -22,9 +22,34 @@ def index():
 
     return render_template('results_analysis/results_analysis.html')
 
+@resultsAnalysis.route('/updateList/',methods=['POST','GET'])
+def updateList():
+    result = dict()
+    type_path = os.path.dirname(os.path.realpath(__file__)).replace("resultsAnalysis", "static/res_pictures/result")
+    type_list = os.listdir(type_path)
+    for i in range(1, len(type_list) + 1):
+        result[i] = type_list[i - 1]
+    return result
+
+
+@resultsAnalysis.route('/selectLog/', methods=['POST', 'GET'])
+def selectLog():
+    type = request.args.get('mydata')
+
+    print(type)
+    return str(1)
+
+
+@resultsAnalysis.route('/confirmLog/', methods=['POST', 'GET'])
+def confirmLog():
+    flag = request.args.get('flag')
+    print(flag)
+    ##拿到flag后开始根据这个索引收集数据
 
 
 
+
+    return {"dd":flag}
 
 
 
